e5bfc8ef415c4339ee169154e6700a88fb745ee5
JK3Y <jacobdamador@gmail.com>
2018-04-28 12:02:15 -0500

    Initial Commit

---
+++ NPS Browser/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  NPS Browser
+//
+//  Created by Jacob Amador on 4/28/18.
+//  Copyright © 2018 JK3Y. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
